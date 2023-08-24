7837206e45c0578ebb19d7769e45b45c7575e5d6
ghali-faten <fatenghali@hotmail.fr>
2023-08-24 22:45:22 +0200

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 
-
+console.log("start")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/dist"));
