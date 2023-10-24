d1fe0f45d6b05933802f3d2bf994b46fa5e7cbf3
ghali-faten <fatenghali@hotmail.fr>
2023-10-24 20:10:40 +0200

    ok

--- server/database-mysql/schema.sql
+++ server/database-mysql/schema.sql
@@ -4,7 +4,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-
+-- -----------------------------------------------------
+-- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema school
@@ -27,7 +28,9 @@ CREATE TABLE IF NOT EXISTS `school`.`teacher` (
   `department` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`idteacher`),
   UNIQUE INDEX `idteacher_UNIQUE` (`idteacher` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -43,10 +46,10 @@ CREATE TABLE IF NOT EXISTS `school`.`courses` (
   INDEX `fk_courses_teacher1_idx` (`teacher_idteacher` ASC) VISIBLE,
   CONSTRAINT `fk_courses_teacher1`
     FOREIGN KEY (`teacher_idteacher`)
-    REFERENCES `school`.`teacher` (`idteacher`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `school`.`teacher` (`idteacher`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -58,23 +61,59 @@ CREATE TABLE IF NOT EXISTS `school`.`student` (
   `age` INT NOT NULL,
   `phone` INT NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-  `teacher_idteacher` INT NOT NULL,
-  `courses_idcourses` INT NOT NULL,
   PRIMARY KEY (`idstudent`),
-  UNIQUE INDEX `idstudent_UNIQUE` (`idstudent` ASC) VISIBLE,
-  INDEX `fk_student_teacher_idx` (`teacher_idteacher` ASC) VISIBLE,
-  INDEX `fk_student_courses1_idx` (`courses_idcourses` ASC) VISIBLE,
-  CONSTRAINT `fk_student_teacher`
-    FOREIGN KEY (`teacher_idteacher`)
-    REFERENCES `school`.`teacher` (`idteacher`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_student_courses1`
+  UNIQUE INDEX `idstudent_UNIQUE` (`idstudent` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `school`.`courses_has_student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `school`.`courses_has_student` (
+  `courses_idcourses` INT NOT NULL,
+  `student_idstudent` INT NOT NULL,
+  PRIMARY KEY (`courses_idcourses`, `student_idstudent`),
+  INDEX `fk_courses_has_student_student1_idx` (`student_idstudent` ASC) VISIBLE,
+  INDEX `fk_courses_has_student_courses1_idx` (`courses_idcourses` ASC) VISIBLE,
+  CONSTRAINT `fk_courses_has_student_courses1`
     FOREIGN KEY (`courses_idcourses`)
     REFERENCES `school`.`courses` (`idcourses`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_courses_has_student_student1`
+    FOREIGN KEY (`student_idstudent`)
+    REFERENCES `school`.`student` (`idstudent`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `school`.`student_has_teacher`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `school`.`student_has_teacher` (
+  `student_idstudent` INT NOT NULL,
+  `teacher_idteacher` INT NOT NULL,
+  PRIMARY KEY (`student_idstudent`, `teacher_idteacher`),
+  INDEX `fk_student_has_teacher_teacher1_idx` (`teacher_idteacher` ASC) VISIBLE,
+  INDEX `fk_student_has_teacher_student1_idx` (`student_idstudent` ASC) VISIBLE,
+  CONSTRAINT `fk_student_has_teacher_student1`
+    FOREIGN KEY (`student_idstudent`)
+    REFERENCES `school`.`student` (`idstudent`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_student_has_teacher_teacher1`
+    FOREIGN KEY (`teacher_idteacher`)
+    REFERENCES `school`.`teacher` (`idteacher`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
